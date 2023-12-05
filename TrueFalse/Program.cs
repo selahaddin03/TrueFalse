220d1d97e3414468f1e998b824a19b92cf2e1833
selahaddin03 <149465780+selahaddin03@users.noreply.github.com>
2023-12-05 19:37:03 +0300

    Proje dosyası ekle.

---
+++ TrueFalse/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrueFalse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           SayiTekmiCiftMi sayibul = new SayiTekmiCiftMi();
+            bool sonuc= sayibul.TekCift(1);
+            if( sonuc )
+            {
+                Console.WriteLine("Sayı tektir");
+            }
+            else
+            {
+                Console.WriteLine("sayı çifttir");
+            }
+            
+
+        }
+    }
+    public class SayiTekmiCiftMi
+    {
+        public bool TekCift(int sayi)
+        {
+            if (sayi % 2 == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
